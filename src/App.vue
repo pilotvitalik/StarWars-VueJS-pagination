4c0e9c5c9dea2ab7009dec38405b82e62a6b71c5
pilotvitalik <itvkip@yandex.ru>
2019-10-09 13:32:37 +0300

    Создан компонент Footer, статический

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,20 @@
   <div id="app">
    <appHeader></appHeader>
    <Common></Common>
+   <appFooter></appFooter>
   </div>
 </template>
 
 <script>
 import Header from './components/Header.vue';
 import Common from './components/Common.vue';
+import Footer from './components/Footer.vue';
 
 export default {
   components: {
     appHeader: Header,
     Common: Common,
+    appFooter: Footer,
   },
   data() {
     return {
---
+++ src/components/Footer.vue
@@ -0,0 +1,39 @@
+<template>
+	<footer id='footer'>
+		<h3>STAR WARS CHARACTER Encyclopedia, 2019</h3>
+	</footer>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+    };
+  },
+};
+</script>
+
+
+<style lang='less' scoped>
+#footer{
+	display: flex;
+	flex-direction: column;
+	position: relative;
+	justify-content: center;
+	align-items: center;
+	width: 100%;
+	height: 120px;
+	background: #1a1a1a;
+	h3{
+		display: inline-block;
+		position: relative;
+		color: #fff;
+		font-family: 'Roboto',sans-serif;
+		font-weight: bold;
+		font-size: 18px;
+		line-height: 21px;
+		letter-spacing: 0.11em;
+		text-transform: uppercase;
+	}
+}
+</style>
